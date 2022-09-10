5691ecac122e9d96d39e8436f16e2fc18f0efe22
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-09-10 21:54:09 +0300

    111

---
+++ src/js/task-01.js
@@ -0,0 +1,5 @@
+const categoriesRef = document.querySelector('.categories');
+console.log(categories); 
+const listItems = document.querySelectorAll('.item');
+console.log(listItems);
+/*listItems.forEach((item)=>{console.lof(item);});*/
