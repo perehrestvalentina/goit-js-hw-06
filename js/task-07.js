113cb8ff96eee441edede261fb5f35a9385be435
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-09-12 01:11:02 +0300

    add 8task

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
-let controlFont = document.getElementById("font-size-control");
+const controlFont = document.getElementById("font-size-control");
 
-let text = document.getElementById("text");
+const text = document.getElementById("text");
 
 controlFont.oninput = function() {
   text.style.fontSize = controlFont.value + "px";
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+
+const result = {
+  changeColorBtn: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+  body: document.querySelector('body'), 
+};
+
+result.changeColorBtn.addEventListener('click', changeColorBtn);
+
+function changeBtnClick(){
+  const currentColor = getRandomHexColor();
+result.color.textContent = currentColor;
+result.style.backgroundColor= currentColor; 
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
