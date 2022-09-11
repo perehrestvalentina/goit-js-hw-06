d431bc0e558ef354219ecc8f0ca479cf282de10b
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-09-12 00:00:34 +0300

    add 5task

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,18 @@
-let input = document.getElementById("name-input");
-let nameOutput = document.getElementById("name-output");
+// let input = document.getElementById("name-input");
+// let nameOutput = document.getElementById("name-output");
 
-input.oninput = function () {
-    if (input.value === '') {
-       nameOutput.innerHTML = 'Anonymous';
-    } else{
-    nameOutput.innerHTML = input.value;
-    }
-}
+// input.oninput = function () {
+//     if (input.value === '') {
+//        nameOutput.innerHTML = 'Anonymous';
+//     } else{
+//     nameOutput.innerHTML = input.value;
+//     }
+// }
+const inputNameRef= document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output')
+
+const handleInputName = (event)=>{
+    const{value}= event.target;
+    outputRef.textContent = value? value : 'Anonymous';
+};
+inputNameRef.addEventListener('input',handleInputName);
